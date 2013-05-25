a523aebf70fabc35e591f4b9e9caa62e42c8c1e5
Negated <youhavethewrong@gmail.com>
2013-05-25 16:41:19 -0500

    Added readme and a few ideas to implement.

---
+++ README.md
@@ -0,0 +1,3 @@
+A repository for exercises pursuant to my goal: Always Be Coding.
+
+ESC 2013.05.25
---
+++ python/mergeSort.py
@@ -0,0 +1,35 @@
+"""
+Top-down merge sort implementation.
+"""
+
+def merge(left, right):
+    result = []
+    while len(left) > 0 or len(right) > 0:
+        if(len(left) > 0 and len(right) > 0):
+            if(left[0] < right[0]):
+                result.append(left[0])
+                left = left[1:]
+            else:
+                result.append(right[0])
+                right = right[1:]
+        elif(len(left) > 0):
+            result.append(left[0])
+            left = left[1:]
+        elif(len(right) > 0):
+            result.append(right[0])
+            right = right[1:]
+
+    return result
+
+def mergesort(sortable_list):
+    if(len(sortable_list) <= 1):
+        return sortable_list
+
+    left = mergesort(sortable_list[len(sortable_list)/2:])
+    right = mergesort(sortable_list[:len(sortable_list)/2])
+
+    return merge(left, right)
+
+
+if __name__ == '__main__':
+    print mergesort([0, 9, 8, 7, 6, 5, 4, 3, 2, 1, 1337])
